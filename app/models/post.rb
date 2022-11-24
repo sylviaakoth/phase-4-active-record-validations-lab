83a4bef2dab916d8e6061cb11261521a2fba6c53
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-11-24 11:20:47 +0300

    still working on some blockers

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: {within: %w(Fiction Non-Fiction)}
+    validates :content, length: {minimum: 250}
 end
