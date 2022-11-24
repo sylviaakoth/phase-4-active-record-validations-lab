72a2e0b80ac12bc40295385c59930b19f9f1c07a
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-11-24 11:20:12 +0300

    commit tree 73ad6d85a4129401972317b072d17b3104aadc4a

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,26 @@
+class AuthorsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_response
+      
+      def show
+        author = Author.find(params[:id])
+    
+        render json: author
+      end
+    
+      def create
+        author = Author.create!(author_params)
+    
+        render json: author, status: :created
+      end
+    
+      private
+      
+      def author_params
+        params.permit(:email, :name)
+      end
+    
+      def render_unprocessable_response(invalid)
+        render json: {errors: invalid.record.errors.messages}, status: :unprocessable_entity
+      end
+      
+    end
